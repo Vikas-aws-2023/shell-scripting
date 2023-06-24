ee2a73378d877bc797ce178d940815abc09972c9
Vikas-aws-2023 <awsanddevops.2023@gmail.com>
2023-06-24 15:49:17 +0530

    update-message

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -1,1 +1,25 @@
-cghgvhgvhhv hvjh
+#!/bin/bash
+
+COMPONENT=catalogue
+LOGFILE="/tmp/${COMPONENT}.log"
+ID=$(id -u)
+
+
+if [ $ID -ne 0 ]; then
+echo "This action perform only root user"
+exit 1
+fi
+
+status(){
+
+    if [ $1 -eq 0 ]; then
+        echo "Success"
+    else
+        echo "Failure"
+    fi
+}
+
+echo "Configur the ${COMPONENT} repo"
+
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+status $?
