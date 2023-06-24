40f55ff1e78285b26694818fb2e44709ea171bdb
Vikas-aws-2023 <awsanddevops.2023@gmail.com>
2023-06-24 16:24:15 +0530

    update-message

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -3,6 +3,7 @@
 COMPONENT=catalogue
 LOGFILE="/tmp/${COMPONENT}.log"
 ID=$(id -u)
+APPUSER="roboshop"
 
 
 if [ $ID -ne 0 ]; then
@@ -21,5 +22,32 @@ status(){
 
 echo "Configur the ${COMPONENT} repo"
 
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
 status $?
+
+echo "Install ${COMPONENT}"
+yum install nodejs -y &>> $LOGFILE
+status $?
+
+id ${APPUSER} &>> $LOGFILE
+
+if [ $? ne 0 ]; then
+echo "Create a service account"
+useradd roboshop
+fi
+status $?
+
+echo "Donwloading the component"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+status $?
+
+echo "Copy the ${COMPONENT} to ${APPUSER} home directory"
+
+cd /home/${APPUSER}
+unzip /tmp/${COMPONENT}.zip
+mv ${COMPONENT}-main ${COMPONENT}
+chown -R ${APPUSER}:${APPUSER} /home/roboshop/${COMPONENT}
+status $?
+
+
+
