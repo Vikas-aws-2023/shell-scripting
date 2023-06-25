986b5289ecc81354cf596f765548f15587dd9c16
Vikas-aws-2023 <awsanddevops.2023@gmail.com>
2023-06-25 16:29:39 +0530

    update-message

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -45,8 +45,9 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
 status $?
 
 
-for component  in catalogue ; do
+for component  in catalogue user ; do
 sed -i -e "/$component/s/localhost/172.31.84.24/"  /etc/nginx/default.d/roboshop.conf
+sed -i -e "/$component/s/localhost/172.31.82.55/"  /etc/nginx/default.d/roboshop.conf
 done
 status $?
 
