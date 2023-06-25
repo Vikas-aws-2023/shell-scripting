90fdcb70658c4e9863306c1fcfbe596236b780b2
Vikas-aws-2023 <awsanddevops.2023@gmail.com>
2023-06-25 16:44:32 +0530

    update-message

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -45,10 +45,9 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
 status $?
 
 
-for component  in catalogue user ; do
-sed -i -e "/$component/s/localhost/172.31.84.24/"  /etc/nginx/default.d/roboshop.conf
-sed -i -e "/$component/s/localhost/172.31.82.55/"  /etc/nginx/default.d/roboshop.conf
-done
+echo "Update the config file"
+sed -i -e "/catalogue/s/localhost/172.31.84.24/" -e "/user/s/localhost/172.31.82.55/" /etc/nginx/default.d/roboshop.conf
+
 status $?
 
 
@@ -57,6 +56,16 @@ systemctl restart nginx
 systemctl enable nginx
 status $?
 echo "Fontend installation completed successfully"
+
+    # location /api/catalogue/ { proxy_pass http://172.31.84.24:8080/; }
+
+    # location /api/user/ { proxy_pass http://172.31.84.24:8080/; }
+
+    # location /api/cart/ { proxy_pass http://localhost:8080/; }
+
+    # location /api/shipping/ { proxy_pass http://localhost:8080/; }
+
+    # location /api/payment/ { proxy_pass http://localhost:8080/; }
 
 
 
