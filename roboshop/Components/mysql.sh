4135cc65834f505b97eba53d61442723e7299806
Vikas-aws-2023 <awsanddevops.2023@gmail.com>
2023-06-26 12:38:03 +0530

    update-message

--- roboshop/Components/mysql.sh
+++ roboshop/Components/mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COMPONENT=mysql
+LOGFILE="/tmp/${COMPONENT}.log"
 
 if [ $ID -ne 0 ]; then
 echo "This action perform only root user"
@@ -17,19 +18,19 @@ status(){
 }
 
 echo "Confifure ${COMPONENT} component"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
 status $?
 
 echo "Install ${COMPONENT} component"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> $LOGFILE
 status $?
 
 echo "Starting ${COMPONENT} component"
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
 status $?
 
 echo "Fetch the default root password"
-DEFAULT_ROOT_PASSWORD= $(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "Default Password is = ${DEFAULT_ROOT_PASSWORD}"
+DEFAULT_ROOT_PASSWORD= $(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}') &>> $LOGFILE
+echo "Default Password is = ${DEFAULT_ROOT_PASSWORD}" 
 status $?
